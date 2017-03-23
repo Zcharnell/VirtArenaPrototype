17da1c211d798e24713b32a57c36baaa69b483f2
zcharnell <zcharnell@gmail.com>
2017-03-23 15:07:30 -0700

    Update server to use ENV_VARIABLE

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 // var express = require('express'),
 // 	app = express();
 var ENV_VARIABLE = process.env.ENV_VARIABLE || 'dev';
-console.log(ENV_VARIABLE);
+var ENV_DIR = '/' + ENV_VARIABLE;
+
 var express = require('express');
 var app = express();
 // var mongoose = require('mongoose');
@@ -14,26 +15,27 @@ var methodOverride = require('method-override');
 var errorhandler = require('errorhandler');
 var server = require('http').createServer(app);
 
-app.use(express.static(__dirname + '/dev'));
-app.use('/js', express.static(__dirname + '/dev/js'));
-app.use('/assets', express.static(__dirname + '/dev/assets'));
-app.use('/sass', express.static(__dirname + '/dev/sass'));
-app.use('/css', express.static(__dirname + '/dev/css'));
-app.use('/templates', express.static(__dirname + '/dev/templates'));
-app.use('/json', express.static(__dirname + '/dev/json'));
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + ENV_DIR));
+app.use('/js', express.static(__dirname + ENV_DIR + '/js'));
+app.use('/assets', express.static(__dirname + ENV_DIR + '/assets'));
+app.use('/sass', express.static(__dirname + ENV_DIR + '/sass'));
+app.use('/css', express.static(__dirname + ENV_DIR + '/css'));
+app.use('/templates', express.static(__dirname + ENV_DIR + '/templates'));
+app.use('/json', express.static(__dirname + ENV_DIR + '/json'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/jquery', express.static(__dirname + '/jquery'));
 
-app.use(morgan('dev'));                                         // log every request to the console
+app.use(morgan(ENV_VARIABLE));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
-app.use(errorhandler({
-  dumpExceptions: true,
-  showStack: true
-}));
+if(ENV_VARIABLE === 'dev') {
+	app.use(errorhandler({
+	  dumpExceptions: true,
+	  showStack: true
+	}));
+}
 
 // app.get('/',function(req,res){
 // 	res.sendFile('index.html',{'root':__dirname + '/dev'});
